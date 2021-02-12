61cd751476af0f768136ae708838e11bb35d0487
Da-Black-Man <dablackoo@gmail.com>
2021-02-12 03:32:32 -0700

    random

--- src/scripts/modules.js
+++ src/scripts/modules.js
@@ -3,3 +3,4 @@ export { default as Scroll } from "./modules/Scroll";
 export { default as Navigation } from "./modules/Navigation";
 export { default as Social } from "./modules/Social";
 export { default as Progress } from "./modules/Progress";
+export { default as Accordion } from "./modules/Accordion";
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,42 +1,46 @@
-const Path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const TerserJSPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-
-
+const Path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: Path.resolve(__dirname, '../src/scripts/app.js')
+    app: Path.resolve(__dirname, "../src/scripts/app.js"),
   },
   output: {
-    path: Path.join(__dirname, '../build'),
-    filename: 'scripts/[name].js'
+    path: Path.join(__dirname, "../build"),
+    filename: "scripts/[name].js",
   },
   optimization: {
-    minimizer: [new TerserJSPlugin(), new OptimizeCSSAssetsPlugin({
-      assetNameRegExp: /\.optimize\.css$/g,
-      cssProcessor: require('cssnano'),
-      cssProcessorPluginOptions: {
-        preset: ['default', {
-          discardComments: { removeAll: true },
-          minifyFontValues: { removeQuotes: false }
-        }],
-      },
-      canPrint: true
-    })],
+    minimizer: [
+      new TerserJSPlugin(),
+      new OptimizeCSSAssetsPlugin({
+        assetNameRegExp: /\.optimize\.css$/g,
+        cssProcessor: require("cssnano"),
+        cssProcessorPluginOptions: {
+          preset: [
+            "default",
+            {
+              discardComments: { removeAll: true },
+              minifyFontValues: { removeQuotes: false },
+            },
+          ],
+        },
+        canPrint: true,
+      }),
+    ],
     splitChunks: {
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
           name: "vendors",
           chunks: "all",
-          enforce: true
-        }
+          enforce: true,
+        },
       },
-      chunks: "all"
+      chunks: "all",
     },
     // splitChunks: {
     //   chunks: 'all',
@@ -45,83 +49,87 @@ module.exports = {
 
     // TODO: Figure out why prod app.js shows app.chunck.js when runtimeChunck is enabled....
 
-
     runtimeChunk: {
-      name: "runtime"
-    }
+      name: "runtime",
+    },
   },
 
   plugins: [
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin([
-        { from: Path.resolve(__dirname, '../src/assets'), to: 'assets' }
+      { from: Path.resolve(__dirname, "../src/assets"), to: "assets" },
     ]),
     new HtmlWebpackPlugin({
-      title: 'Home | Boilerplate',
-      filename: 'index.html',
-      minify:  false,
-      template: Path.resolve(__dirname, '../src/index.html')
+      title: "Home | Boilerplate",
+      filename: "index.html",
+      minify: false,
+      template: Path.resolve(__dirname, "../src/index.html"),
     }),
     new HtmlWebpackPlugin({
-      title: 'Page | Boilerplate',
-      filename: 'page.html',
-      minify:  false,
-      template: Path.resolve(__dirname, '../src/page.html')
+      title: "Driver | Boilerplate",
+      filename: "driver.html",
+      minify: false,
+      template: Path.resolve(__dirname, "../src/driver.html"),
     }),
     new HtmlWebpackPlugin({
-      title: 'Image | Boilerplate',
-      filename: 'image.html',
-      minify:  false,
-      template: Path.resolve(__dirname, '../src/image.html')
-    })
+      title: "Warehouse | Boilerplate",
+      filename: "warehouse.html",
+      minify: false,
+      template: Path.resolve(__dirname, "../src/warehouse.html"),
+    }),
   ],
   resolve: {
     alias: {
-      '~': Path.resolve(__dirname, '../src')
-    }
+      "~": Path.resolve(__dirname, "../src"),
+    },
   },
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.mjs$/,
         include: /node_modules/,
-        type: 'javascript/auto'
+        type: "javascript/auto",
       },
       {
         test: /\.html$/,
         use: {
-          loader: 'html-loader',
+          loader: "html-loader",
           options: {
             interpolate: true,
             minimize: false,
-          }
-        }
+          },
+        },
       },
       {
         test: /\.(gif|jpg|png|jpe?g)$/i,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            useRelativePath: true,
-            outputPath: 'assets/images',
-            publicPath: '',
-            esModule: false
-          }
-        }]
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              useRelativePath: true,
+              outputPath: "assets/images",
+              publicPath: "",
+              esModule: false,
+            },
+          },
+        ],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf|svg)$/i,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            useRelativePath: true,
-            outputPath: 'assets/fonts',
-            publicPath: '',
-            esModule: false
-          }
-        }]
-      }
-    ]
-  }
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              useRelativePath: true,
+              outputPath: "assets/fonts",
+              publicPath: "",
+              esModule: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
